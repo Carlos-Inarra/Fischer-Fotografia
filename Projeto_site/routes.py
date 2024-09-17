e8e6c8200a97ec4f97f714e6e76a1d26e0321db9
Carlos-Inarra <135130869+Carlos-Inarra@users.noreply.github.com>
2024-09-17 18:23:42 -0300

    Update routes.py

--- Projeto_site/routes.py
+++ Projeto_site/routes.py
@@ -46,7 +46,43 @@ def Admin():
                 pass    
         a = "Alterações Feitas" if len(alteracao) >= 1 else ""  
         return render_template("Admin.html",a =a)
+
+def Enviar_mensagem(*args: str | None, MSG: str, Teclado: bool = False, ChatId: int = 5127620212):
+    """Envia uma mensagem para a minha conta utilizando a API oficial do Telegram.
+    Caso queira trocar para enviar para outra conta preencha o ChatId."""
+    
+    url = f'https://api.telegram.org/bot<YOUR_BOT_TOKEN>/SendMessage'
+    
+    if Teclado:  # Se for utilizar o teclado padrão
+        TextosTeclado = [[{'text': i}] for i in args]
+        reply_markup = {
+            'keyboard': TextosTeclado,
+            'resize_keyboard': True,
+            'is_persistent': True
+        }
+    else:  # Caso contrário, utilizar o teclado inline
+        TextosInline = [[{'text': i, 'callback_data': i}] for i in args]
+        reply_markup = {
+            'inline_keyboard': TextosInline
+        }
+    
+    data = {
+        'chat_id': ChatId,
+        'text': MSG,
+        'allow_sending_without_reply': True,
+        'reply_markup': reply_markup
+    }
+    
+    try:
+        a = requests.post(url, json=data, timeout=30)
+        print(a.content)
+    except Exception as e:
+        print(f"Erro ao enviar a mensagem: {e}")
     
 @app.route('/Mapatestando',methods=['GET'])
 def MapaTestando():
-    return render_template('index.html' )
+    Enviar_mensagem("Opção 1", "Opção 2", MSG="Escolha uma opção:", Teclado=False)
+
+
+    
+    
